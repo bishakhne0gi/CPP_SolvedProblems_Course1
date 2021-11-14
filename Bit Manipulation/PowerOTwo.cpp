cca1d75217bd415b139200b8b3690361d0a329f2
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2021-11-14 12:51:04 +0530

    Bit Manipulation

--- Bit Manipulation/PowerOTwo.cpp
+++ Bit Manipulation/PowerOTwo.cpp
@@ -7,6 +7,6 @@ int main(){
     if((n & (n-1))==0)
         cout<<"Power of two"<<endl;
     else
-        cout<<"Not Power of two"<<endl;
+        cout<<"Not Power of two "<<endl;
     return 0;
 }
