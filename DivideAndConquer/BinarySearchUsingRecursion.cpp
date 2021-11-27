b4fb6c3638b244af69b5979435641280838cb32f
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2021-11-27 19:33:39 +0530

    Divide and Conquer

--- DivideAndConquer/BinarySearchUsingRecursion.cpp
+++ DivideAndConquer/BinarySearchUsingRecursion.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int binary(vector<int> arr, int l, int r, int x)
 {
     if (r >= l) {
-        int mid = l + (r - l) / 2;
+        int mid = l + r  / 2;
   
         // If the element is present at the middle
         // itself
