3134a7a01f3670d23baf9fae68d58268892af5f7
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-04-06 00:10:01 +0530

    heap

--- Heap/HeapSort.cpp
+++ Heap/HeapSort.cpp
@@ -2,34 +2,35 @@
 using namespace std;
 void heapify(int arr[], int n, int i)
 {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+    if(left<n && arr[left]>arr[largest])
+        largest=left;
+    if(right<n && arr[right]>arr[largest])
+        largest=right;
     if(largest!=i)
     {
         swap(arr[i],arr[largest]);
         heapify(arr,n,largest);
     }
+
 }
 void heapsort(int arr[], int n)
 {
-    for (int i = n / 2 - 1; i >= 0; i--)
-    {
-        heapify(arr, n, i);
-    }
-    for(int i=n-1; i>=0; i--)
-    {
-        swap(arr[0], arr[i]);
-        heapify(arr,i,0);
-    }
+   for(int i=n/2-1; i>=0; i--)
+   {
+       heapify(arr,n,i);
+   } 
+   for(int i=n-1; i>=0; i--)
+   {
+       swap(arr[i],arr[0]);
+       heapify(arr,i,0);
+   }
 }
-void print(int arr[],int n)
+void print(int arr[], int n)
 {
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n ;i++)
     {
         cout<<arr[i]<<" ";
     }
@@ -37,11 +38,9 @@ void print(int arr[],int n)
 }
 int main()
 {
-    int arr[] = {10, 15, 21, 30, 18, 19};
-    int n = sizeof(arr) / sizeof(int);
-
-    heapsort(arr, n);
-
+    int arr[]={12,10,6,8,9};
+    int n= sizeof(arr)/sizeof(int);
+    heapsort(arr,n);
     print(arr,n);
     return 0;
 }
