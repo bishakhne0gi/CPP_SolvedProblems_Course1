ca97713d41d12fd35ff1065a3b724e527c887b74
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-04-06 17:17:49 +0530

    Queue

--- Queue/QueueImplementation.cpp
+++ Queue/QueueImplementation.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Queue
 {
@@ -8,43 +8,75 @@ class Queue
     int front;
     int rear;
 
-    public:
-        Queue(int default_size=5)
+public:
+    Queue(int default_size = 5)
+    {
+        ms = default_size;
+        arr = new int[ms];
+        cs = 0;
+        front = 0;
+        rear = ms - 1;
+    }
+
+    bool full()
+    {
+        return cs == ms;
+    }
+    bool empty()
+    {
+        return cs == 0;
+    }
+    void push(int data)
+    {
+        if (!full())
         {
-            ms=default_size;
-            arr= new int[ms];
-            cs=0;
-            front=0;
-            rear=ms-1;
+            rear = (rear + 1) % ms;
+            arr[rear] = data;
+            cs++;
         }
-      
-        bool full()
+    }
+    int frontFun()
+    {
+        if (!empty())
+            return arr[front];
+        return 0;
+    }
+    void pop()
+    {
+        if (!empty())
         {
-            return cs==ms;
+            front = (front + 1) % ms;
+            cs--;
         }
-        bool empty()
-        {
-            return cs==0;
-        }
-          void push(int data)
-        {
-            if(!full())
-            {
-                rear=(rear+1)%ms;
-                arr[rear]=data;
-                cs++;
-            }
-        }
-        int front()
-        {
-            return arr[rear];
-        }
-        void pop()
-        {
-            if(!empty())
-            {
-                front=(front+1)%ms;
-                cs--;
-            }
-        }
+    }
 };
+int main()
+{
+    Queue q;
+    int choice;
+    do
+    {
+        cout << "Enter the choice: " << endl;
+        cout << "1.Push 2.Pop 3.Front 4.Exit" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            int d;
+            cout << "Enter the element: " << endl;
+            cin >> d;
+            q.push(d);
+            break;
+        case 2:
+            q.pop();
+            break;
+        case 3:
+            cout << "Front Element: ";
+            cout << q.frontFun() << endl;
+            break;
+        default:
+            cout << "Enter correct choice" << endl;
+        }
+    } while (choice != 4);
+    return 0;
+}
