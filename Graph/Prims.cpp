50cc9c9c3e65a25747946b7db9f167f2fe8d246b
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-04-19 18:55:08 +0530

    Graph

--- Graph/Prims.cpp
+++ Graph/Prims.cpp
@@ -1,27 +1,68 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 class Graph
 {
-    int V;
     vector<pair<int,int>> *l;
+    int V;
     public:
-        Graph(int v)
-        {
-            V=v;
-            l = new vector<pair<int,int>> [v];
-        }
-        void addEdge(int i, int j, int w)
-        {
-            l[i].push_back({j,w});
-            l[j].push_back({i,w});
+    Graph(int v)
+    {
+        V=v;
+        l= new vector<pair<int,int>> [v];
+    }
+    void addEdge(int x, int y ,int w)
+    {
+        l[x].push_back({y,w});
+        l[y].push_back({x,w});
+    }
+    int prim_mst()
+    {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> Q;
+        bool *visited= new bool[V]{0};
+        int ans=0;
 
+        //begin
+
+        Q.push({0,0});//weight,node
+        while(!Q.empty())
+        {
+            auto best= Q.top();
+            Q.pop();
+
+            int to = best.second;
+            int weight = best.first;
+
+            if(visited[to]== true)
+            {
+                continue;
+            }
+            ans+=weight;
+            visited[to]=true;
+            //add the new edges in the priority queue
+            for(auto x: l[to])
+            {
+                if(visited[x.first]==0)
+                    {
+                        Q.push({x.second,x.first});
+                    }
+            }
         }
-        int prim_mst()
-        [
-            priority_queue<
-        ]
+        return ans;
+    }
 };
 int main()
 {
+    Graph g(6);
+    g.addEdge(0,1,7);
+    g.addEdge(0,3,8);
+    g.addEdge(1,3,3);
+    g.addEdge(1,2,6);
+    g.addEdge(3,2,4);
+    g.addEdge(3,4,3);
+    g.addEdge(2,4,2);
+    g.addEdge(2,5,5);
+    g.addEdge(4,5,2);
+    cout<<"COST OF MINIMUM SPANNING TREE: "<<endl;
+    cout<<g.prim_mst()<<endl;
     return 0;
 }
