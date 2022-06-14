df7e2e50b68e70dfb526a1ff91b7cfd9bdb7ee13
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-06-14 12:53:29 +0530

    DAA LAB

--- DAA LAB/Heap/HeapSort.cpp
+++ DAA LAB/Heap/HeapSort.cpp
@@ -7,31 +7,36 @@ using namespace std;
 int arr[100];
 void heapify(int arr[], int n, int i)
 {
-    int largest=i;
-    int left=2*i+1;
-    int right=2*i+2;
-    if(left<n && arr[left]>arr[largest])
-        largest=left;
-    if(right<n && arr[right]>arr[largest])
-        largest=right;
-    if(largest!=i)
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+    if (largest != i)
     {
-        swap(arr[i],arr[largest]);
-        heapify(arr,n,largest);
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
     }
-
 }
 void heapsort(int arr[], int n)
 {
-   for(int i=n/2-1; i>=0; i--)
-   {
-       heapify(arr,n,i);
-   } 
-   for(int i=n-1; i>=0; i--)
-   {
-       swap(arr[i],arr[0]);
-       heapify(arr,i,0);
-   }
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr[i], arr[0]);
+        cout << "Steps: " << endl;
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[j] << "  ";
+        }
+        cout << endl;
+        heapify(arr, i, 0);
+    }
 }
 int main()
 {
@@ -49,7 +54,8 @@ int main()
         cout << arr[i] << "  ";
     }
     cout << endl;
-    heapsort(arr,n);
+    heapsort(arr, n);
+    cout<<endl;
     cout << "Sorted Array" << endl;
     for (int i = 0; i < n; i++)
     {
